09fd3f0baa213511e115e4c4477da2bc8b040db0
Eugene <zukaatoku@gmail.com>
2021-05-22 14:03:23 +0500

    footer github logo

--- src/Opravilo.Web/app/src/layouts/AnonymousLayout.tsx
+++ src/Opravilo.Web/app/src/layouts/AnonymousLayout.tsx
@@ -1,6 +1,7 @@
 import {FunctionComponent} from "react";
 import {Layout} from "antd";
 import * as React from "react";
+import GithubLogo from "../components/GithubLogo/GithubLogo";
 const { Header, Footer, Content } = Layout;
 
 export const AnonymousLayout: FunctionComponent = (props) => (
@@ -9,6 +10,6 @@ export const AnonymousLayout: FunctionComponent = (props) => (
         <Content style={{padding: 24, margin: 0, minHeight: 280 }} >
             {props.children}
         </Content>
-        <Footer>Footer</Footer>
+        <Footer><GithubLogo prompt="Project repository" url="https://github.com/ejenin/Opravilo"/></Footer>
     </Layout>
 );
