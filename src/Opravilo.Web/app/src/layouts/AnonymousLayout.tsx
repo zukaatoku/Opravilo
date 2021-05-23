f7bed162611a1f116dd2391c4611e305c3048273
Eugene <zukaatoku@gmail.com>
2021-05-23 10:47:49 +0500

    added fancy logo

--- src/Opravilo.Web/app/src/layouts/AnonymousLayout.tsx
+++ src/Opravilo.Web/app/src/layouts/AnonymousLayout.tsx
@@ -4,10 +4,13 @@ import {Layout} from "antd";
 import * as React from "react";
 import GithubLogo from "../components/GithubLogo/GithubLogo";
 const { Header, Footer, Content } = Layout;
+import Logo from "../assets/logo.svg";
 
 export const AnonymousLayout: FunctionComponent = (props) => (
     <Layout style={{height: "100vh"}}>
-        <Header>Header</Header>
+        <Header>
+            <div className="headerText"><img src={Logo} alt="Logo"/> OPRAVILO</div> 
+        </Header>
         <Content style={{padding: 24, margin: 0, minHeight: 280 }} >
             {props.children}
         </Content>
