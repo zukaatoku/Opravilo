4c8875d5f8f10505e416850914e5b6de8595bfe2
Eugene <zukaatoku@gmail.com>
2021-06-14 12:23:44 +0500

    footer moved

--- src/Opravilo.Web/app/src/layouts/AnonymousLayout/AnonymousLayout.tsx
+++ src/Opravilo.Web/app/src/layouts/AnonymousLayout/AnonymousLayout.tsx
@@ -2,24 +2,22 @@ import "./style.scss";
 import {FunctionComponent} from "react";
 import {Layout} from "antd";
 import * as React from "react";
-import GithubLogo from "../../components/GithubLogo/GithubLogo";
-const { Header, Footer, Content } = Layout;
+const { Header, Content } = Layout;
 import Logo from "../../assets/logo.svg";
+import CustomFooter from "../../components/CustomFooter/CustomFooter";
 
 const AnonymousLayout: FunctionComponent = (props) => (
     <Layout style={{height: "100vh"}}>
         <Header>
-            <div>
+            <a href="/home">
                 <img src={Logo} alt="Logo" className="headerLogo"/>
                 <span className="headerText">OPRAVILO</span>
-            </div>
+            </a>
         </Header>
         <Content style={{padding: 24, margin: 0, minHeight: 280 }} >
             {props.children}
         </Content>
-        <Footer>
-            <GithubLogo prompt="Project repository" url="https://github.com/ejenin/Opravilo" cssClass="hoverableTransition"/>
-        </Footer>
+        <CustomFooter />
     </Layout>
 );
 
--- src/Opravilo.Web/app/src/layouts/UserLayout/UserLayout.tsx
+++ src/Opravilo.Web/app/src/layouts/UserLayout/UserLayout.tsx
@@ -1,29 +1,26 @@
 import {FunctionComponent} from "react";
 import {Layout} from "antd";
 import Logo from "../../assets/logo.svg";
-import GithubLogo from "../../components/GithubLogo/GithubLogo";
-const { Header, Footer, Content } = Layout;
+const { Header, Content } = Layout;
 import * as React from "react";
 import AuthManager from "../../auth/AuthManager";
+import CustomFooter from "../../components/CustomFooter/CustomFooter";
 
 const UserLayout: FunctionComponent = (props) => {
     const userName = AuthManager.getDisplayName();
 
     return (<Layout style={{height: "100vh"}}>
         <Header>
-            <div>
+            <a href="/home">
                 <img src={Logo} alt="Logo" className="headerLogo"/>
-                <span className="headerText">USER LAYOUT</span>
-            </div>
-            <div style={{float: "right"}}>{userName}</div>
+                <span className="headerText">OPRAVILO</span>
+            </a>
+            <div style={{float: "right", color:"white"}}>{userName}</div>
         </Header>
         <Content style={{padding: 24, margin: 0, minHeight: 280}}>
             {props.children}
         </Content>
-        <Footer>
-            <GithubLogo prompt="Project repository" url="https://github.com/ejenin/Opravilo"
-                        cssClass="hoverableTransition"/>
-        </Footer>
+        <CustomFooter />
     </Layout>)
 }
 
