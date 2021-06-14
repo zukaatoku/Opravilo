d71b8c2a0c48c8bd8cc9329fba9a19b4d9408588
Eugene <zukaatoku@gmail.com>
2021-06-14 13:41:10 +0500

    user layoput tweaks

--- src/Opravilo.Web/app/src/layouts/UserLayout/UserLayout.tsx
+++ src/Opravilo.Web/app/src/layouts/UserLayout/UserLayout.tsx
@@ -5,17 +5,34 @@ const { Header, Content } = Layout;
 import * as React from "react";
 import AuthManager from "../../auth/AuthManager";
 import CustomFooter from "../../components/CustomFooter/CustomFooter";
+import { Menu } from 'antd';
+import {UserOutlined} from "@ant-design/icons";
+import {useHistory} from "react-router-dom";
+
+const { SubMenu } = Menu;
 
 const UserLayout: FunctionComponent = (props) => {
     const userName = AuthManager.getDisplayName();
-
+    const history = useHistory();
+    
+    const onLogout = () => {
+        AuthManager.removeTokens();
+        history.push("/");
+    };
+    
+    // todo: avatar
+    
     return (<Layout style={{height: "100vh"}}>
         <Header>
             <a href="/home">
                 <img src={Logo} alt="Logo" className="headerLogo"/>
                 <span className="headerText">OPRAVILO</span>
             </a>
-            <div style={{float: "right", color:"white"}}>{userName}</div>
+            <Menu mode="horizontal" theme="dark" style={{float: "right"}}>
+                <SubMenu title={userName} icon={<UserOutlined/>}>
+                    <Menu.Item onClick={onLogout} danger>Logout</Menu.Item>
+                </SubMenu>
+            </Menu>
         </Header>
         <Content style={{padding: 24, margin: 0, minHeight: 280}}>
             {props.children}
--- src/Opravilo.Web/app/src/pages/home/HomePage.tsx
+++ src/Opravilo.Web/app/src/pages/home/HomePage.tsx
@@ -1,20 +1,9 @@
 import * as React from "react";
 import {FC} from "react";
-import {Button} from "antd";
-import AuthManager from "../../auth/AuthManager";
-import {useHistory} from "react-router-dom";
+import {Empty} from "antd";
 
 const HomePage: FC = () => {
-    const history = useHistory();
-    const onLogout = () => {
-        AuthManager.removeTokens();
-        history.push("/");
-    };
-    
-    return (<div>
-        <h1> HOME SWEET HOME 2 </h1>
-        <Button onClick={onLogout}>LOGOUT</Button>
-    </div> )
+    return <Empty />
 };
 
 export default HomePage;
