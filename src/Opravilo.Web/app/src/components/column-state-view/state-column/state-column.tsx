7011ec8cfa69d7441120ad54309b61cd9842c7c3
Eugene <zukaatoku@gmail.com>
2021-11-07 11:05:50 +0500

    header + menu tweaks

--- src/Opravilo.Web/app/src/components/column-state-view/state-column/state-column.tsx
+++ src/Opravilo.Web/app/src/components/column-state-view/state-column/state-column.tsx
@@ -1,16 +1,38 @@
 import React from "react"
 import {IStateColumnProps} from "./types";
-import {Empty} from "antd";
+import {Dropdown, Empty, Menu} from "antd";
+import {EllipsisOutlined} from "@ant-design/icons";
 
 import "./state-column.scss"
 
+const menu = (
+    <Menu>
+        <Menu.Item key="1">
+            1st menu item
+        </Menu.Item>
+        <Menu.Item key="2">
+            2nd menu item
+        </Menu.Item>
+        <Menu.Item key="3">
+            3rd menu item
+        </Menu.Item>
+    </Menu>
+);
+
 const ColumnHeader = (name: string): JSX.Element => {
-    return <h2>{name}</h2>
+    return <header>
+            <h2>{name}</h2>
+            <Dropdown overlay={menu}>
+                <EllipsisOutlined className="dots" />
+            </Dropdown>
+        </header>
 }
 
 export const StateColumn = (props: IStateColumnProps): JSX.Element => {
+    const {name} = props
+    
     return <div className="state-column">
-        {ColumnHeader(props.name)}
+        {ColumnHeader(name)}
         <Empty />
     </div>
 }
