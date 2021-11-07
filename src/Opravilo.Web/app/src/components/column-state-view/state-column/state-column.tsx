b60d19024569aa77eacbfedc523dbc41753f21c4
Eugene <zukaatoku@gmail.com>
2021-11-07 15:58:17 +0500

    more state tweaks

--- src/Opravilo.Web/app/src/components/column-state-view/state-column/state-column.tsx
+++ src/Opravilo.Web/app/src/components/column-state-view/state-column/state-column.tsx
@@ -1,20 +1,17 @@
 import React from "react"
 import {IStateColumnProps} from "./types";
 import {Dropdown, Empty, Menu} from "antd";
-import {EllipsisOutlined} from "@ant-design/icons";
+import {EllipsisOutlined, EditOutlined, DeleteOutlined} from "@ant-design/icons";
 
 import "./state-column.scss"
 
 const menu = (
     <Menu>
-        <Menu.Item key="1">
-            1st menu item
+        <Menu.Item key="1" icon={<EditOutlined />}>
+            Edit
         </Menu.Item>
-        <Menu.Item key="2">
-            2nd menu item
-        </Menu.Item>
-        <Menu.Item key="3">
-            3rd menu item
+        <Menu.Item key="2" icon={<DeleteOutlined />} danger>
+            Remove
         </Menu.Item>
     </Menu>
 );
