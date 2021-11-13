6794567f0ba8cd8caa0e0f55843bd882380f7696
Eugene <zukaatoku@gmail.com>
2021-11-13 12:07:56 +0500

    state button done

--- src/Opravilo.Web/app/src/components/column-state-view/state-column/add-state-column.tsx
+++ src/Opravilo.Web/app/src/components/column-state-view/state-column/add-state-column.tsx
@@ -1,12 +1,8 @@
 import React from "react"
 import "./state-column.scss"
 import {PlusOutlined} from "@ant-design/icons";
-import {Space} from "antd";
+import {Button, Space} from "antd";
 
 export const AddStateColumn = (): JSX.Element => {
-    return <div className="state-column">
-        <Space>
-            <PlusOutlined /> Add state
-        </Space>
-    </div>
+    return <Button icon={<PlusOutlined/>} ghost className="add-state-button" type="dashed">Add state</Button>
 }
