952397d926b885d026147c88e9a7961e0c0bb83c
Eugene <zukaatoku@gmail.com>
2021-11-21 21:25:28 +0500

    card view buttons panel tweaked

--- src/Opravilo.Web/app/src/components/card-view-modal/card-view-modal.tsx
+++ src/Opravilo.Web/app/src/components/card-view-modal/card-view-modal.tsx
@@ -3,8 +3,10 @@ import {Modal} from "antd";
 import { CardViewContainer } from "../../containers/card-view-container";
 import {ICardViewModalProps} from "./types";
 
+import "./card-view-modal.scss"
+
 export const CardViewModal = (props: ICardViewModalProps): JSX.Element => {
-    return <Modal visible={true} onCancel={props.onClose} footer={null} width={800}>
+    return <Modal visible={true} onCancel={props.onClose} footer={null} className="card-view-modal">
         <CardViewContainer />
     </Modal>
 }
--- src/Opravilo.Web/app/src/components/card-view/card-view.tsx
+++ src/Opravilo.Web/app/src/components/card-view/card-view.tsx
@@ -1,7 +1,24 @@
 import React from "react"
 import {ICardViewProps} from "./types";
 
+import "./card-view.scss"
+import {Button, Space} from "antd";
+import {EditOutlined} from "@ant-design/icons";
+
 export const CardView = (props: ICardViewProps): JSX.Element => {
     const {card} = props
-    return <h1>{card.name}</h1>
+    return <div className="card-view">
+        <header><h2>{card.name}</h2></header>
+        <div className="body-wrapper">
+            <div className="description-etc">
+                {card.description}
+            </div>
+            <div className="buttons-panel">
+                <Space direction="vertical" style={{width: "100%"}} className="wrapper" size={0}>
+                    <h4 className="header">Actions</h4>
+                    <Button icon={<EditOutlined />} type="text" block className="button">Edit</Button>
+                </Space>
+            </div>
+        </div>
+    </div>
 }
