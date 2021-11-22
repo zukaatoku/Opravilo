510fc580272994ea1d03b623306d5ded4d6fc00f
Eugene <zukaatoku@gmail.com>
2021-11-22 21:25:08 +0500

    more visual tweaks

--- src/Opravilo.Web/app/src/components/card-view/card-view.tsx
+++ src/Opravilo.Web/app/src/components/card-view/card-view.tsx
@@ -1,9 +1,9 @@
 import React from "react"
 import {ICardViewProps} from "./types";
+import {Button, Space} from "antd";
+import {DeleteOutlined, EditOutlined} from "@ant-design/icons";
 
 import "./card-view.scss"
-import {Button, Space} from "antd";
-import {EditOutlined} from "@ant-design/icons";
 
 export const CardView = (props: ICardViewProps): JSX.Element => {
     const {card} = props
@@ -11,12 +11,16 @@ export const CardView = (props: ICardViewProps): JSX.Element => {
         <header><h2>{card.name}</h2></header>
         <div className="body-wrapper">
             <div className="description-etc">
-                {card.description}
+                <Space direction="vertical">
+                    <span><h4 className="header">Description</h4></span>
+                    <span>{card.description}</span>
+                </Space>
             </div>
             <div className="buttons-panel">
                 <Space direction="vertical" style={{width: "100%"}} className="wrapper" size={0}>
                     <h4 className="header">Actions</h4>
                     <Button icon={<EditOutlined />} type="text" block className="button">Edit</Button>
+                    <Button icon={<DeleteOutlined />} type="text" block danger className="button">Remove</Button>
                 </Space>
             </div>
         </div>
