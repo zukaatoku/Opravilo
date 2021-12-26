fbfaaeef0e7e2240b60525a58be81ef33028e539
Eugene <zukaatoku@gmail.com>
2021-12-26 10:24:59 +0500

    column state to container

--- src/Opravilo.Web/app/src/pages/project-board/project-board-page.tsx
+++ src/Opravilo.Web/app/src/pages/project-board/project-board-page.tsx
@@ -2,9 +2,9 @@ import React, {useEffect} from 'react'
 import {IProjectBoardPageProps} from './types'
 import {Empty, Spin} from 'antd'
 import {Link, withRouter} from 'react-router-dom'
-import {ColumnStateView} from '../../components/column-state-view'
 import {CreateStateFormContainer} from '../../containers/modal/create-state-form-container'
 import {CardViewModal} from '../../components/card-view-modal'
+import {ColumnStateViewContainer} from '../../containers/column-state-view-container'
 
 import './project-board-page.scss'
 
@@ -13,15 +13,9 @@ export const ProjectBoardPage = withRouter((props: IProjectBoardPageProps): JSX.
         fetchingProject,
         currentProject,
         fetchProject,
-        removeState,
         createEditStateVisible,
-        onAddClick,
-        onEditClick,
-        onViewCardClick,
         cardViewModalVisible,
         onCloseCardViewModal,
-        onAddCardClick,
-        onChangeState,
         fetchingChangeState
     } = props
     const id = Number(props.match.params.id)
@@ -29,17 +23,13 @@ export const ProjectBoardPage = withRouter((props: IProjectBoardPageProps): JSX.
     useEffect(() => {
         fetchProject(id)
     }, [fetchProject])
-    
-    // todo ColumnStateView to container
 
     const toShow = currentProject
         ? (
             <>
                 <Link to="/home">Back</Link>
                 <Spin spinning={fetchingChangeState}><h1>{currentProject.name}</h1></Spin>
-                <ColumnStateView states={currentProject.states} onRemove={removeState} 
-                                 onAddStateClick={onAddClick} onEditStateClick={onEditClick} onViewCardClick={onViewCardClick} 
-                                 onAddCardClick={onAddCardClick} onChangeState={onChangeState}/>
+                <ColumnStateViewContainer />
             </>
         )
         : <Empty/>
